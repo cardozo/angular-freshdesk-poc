51bf747a618960544ddd1e82e7719164c39c125e
cardozo <rodrigocardozop@gmail.com>
2021-03-09 18:12:35 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component, VERSION } from "@angular/core";
 import { Http, Headers, RequestOptions } from "@angular/http";
 import "rxjs/add/operator/map";
-import { Status, Types, Priority, Source } from "./freshdesk/freshdesk.commons";
+import {
+  Status,
+  Types,
+  Priority,
+  Source,
+  FreshdeskConfig
+} from "./freshdesk/freshdesk.commons";
 
 @Component({
   selector: "my-app",
@@ -12,15 +18,17 @@ export class AppComponent {
   status: number;
   text: string;
   result: string;
+  ticketId: number;
 
   constructor(private http: Http) {}
 
-  createTicket(file: any) {
+  create(file: any) {
     //https://medgrupo.freshdesk.com/api/v2/ticket_fields
 
     //https://medgrupo.freshdesk.com/api/v2/tickets/613020/
     //https://medgrupo.freshdesk.com/api/v2/tickets/613020/conversations
 
+    //criar entidade
     let obj = {
       source: Source.Email,
       description: "Descrição de teste",
@@ -38,18 +46,13 @@ export class AppComponent {
     //   obj["attachments"] = file.files;
     // }
 
-    // formdata.append("cc_emails", []]);
-    //https://medgrupo.freshdesk.com/api/v2/groups?per_page=100
-
-    //formdata.append("attachments[]", );
-    //formdata.append("custom_fields[matricula]", "123456789");
-    //formdata.append("custom_fields[cf_test_service_worker]", "First Choice");
-
-    this.create(obj).subscribe(
+    this.createTicket(obj).subscribe(
       (res: any) => {
         this.result = "SUCCESS";
         this.status = res.status;
         this.text = JSON.stringify(res._body, null, "<br/>");
+        let body = JSON.parse(res._body);
+        this.ticketId = body.id;
       },
       (error: any) => {
         this.result = "ERROR";
@@ -64,21 +67,15 @@ export class AppComponent {
     );
   }
 
-  create(data) {
-    let yourdomain = "medgrupo";
-    let api_key = "z5Dayu0bzRgkw9x3PuE";
-    // Ref:https://support.freshdesk.com/support/solutions/articles/215517-how-to-find-your-api-key
-    let url = "https://" + yourdomain + ".freshdesk.com/api/v2/tickets";
+  //colocar numa service
+  createReply(file: any) {}
 
-    let headers = new Headers({
-      Authorization: "Basic " + btoa(api_key + ":x")
-    });
-
-    let options = new RequestOptions({
-      headers: headers
-    });
-
-    return this.http.post(url, data, options).map((res: any) => res);
+  //colocar numa service
+  createTicket(data) {
+    let url = FreshdeskConfig.baseURL + "tickets";
+    return this.http
+      .post(url, data, FreshdeskConfig.options)
+      .map((res: any) => res);
   }
 
   getRequestId(error: any) {
--- src/app/freshdesk/freshdesk.commons.ts
+++ src/app/freshdesk/freshdesk.commons.ts
@@ -1,3 +1,5 @@
+import { Http, Headers, RequestOptions } from "@angular/http";
+
 export enum Priority {
   Baixa,
   Media,
@@ -11,6 +13,18 @@ export class Types {
   static informacao: string = "Informação";
   static elogio: string = "Elogio";
   static sugestao: string = "Sugestão";
+}
+
+//Ref:https://support.freshdesk.com/support/solutions/articles/215517-how-to-find-your-api-key
+export class FreshdeskConfig {
+  static domain = "medgrupo";
+  static api_key = "z5Dayu0bzRgkw9x3PuE";
+  static baseURL =
+    "https://" + FreshdeskConfig.domain + ".freshdesk.com/api/v2/";
+  static headers = new Headers({
+    Authorization: "Basic " + btoa(FreshdeskConfig.api_key + ":x")
+  });
+  static options = new RequestOptions({ headers: FreshdeskConfig.headers });
 }
 
 export enum Status {
