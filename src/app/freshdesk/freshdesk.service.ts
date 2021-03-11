7d178e1191f6a648ac1350aec9ae51400d7d7eaa
cardozo <rodrigocardozop@gmail.com>
2021-03-11 13:48:57 -0300

    1 file modified

---
+++ src/app/freshdesk/freshdesk.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router, NavigationEnd } from "@angular/router";
+import {
+  Status,
+  Types,
+  Priority,
+  Source,
+  FreshdeskConfig,
+  Ticket
+} from "./freshdesk.commons";
+
+//@Injectable({ providedIn: 'root' })
+export class FreshdeskService {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private freshdeskConfig: FreshdeskConfig
+  ) {}
+
+  createReplyHTTP(data, ticketId) {
+    let url = FreshdeskConfig.baseURL + "tickets/" + ticketId + "/reply";
+
+    return this.http
+      .post(url, data, FreshdeskConfig.options)
+      .map((res: any) => res);
+  }
+
+  createTicket(data) {
+    let url = FreshdeskConfig.baseURL + "tickets";
+    return this.http
+      .post(url, data, FreshdeskConfig.options)
+      .map((res: any) => res);
+  }
+}
